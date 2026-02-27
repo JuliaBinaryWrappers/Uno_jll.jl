abe1db9134f3af57fa6c5174cdc17fd57757a3a9
jlbuild <juliabuildbot@gmail.com>
2026-02-27 03:51:28 +0000

    Uno_jll build 2.4.0+0

--- src/Uno_jll.jl
+++ src/Uno_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Uno")
-JLLWrappers.@generate_main_file("Uno", UUID("396d5378-14f1-5ab1-981d-48acd51740ed"))
+JLLWrappers.@generate_main_file("Uno", Base.UUID("396d5378-14f1-5ab1-981d-48acd51740ed"))
 end  # module Uno_jll
